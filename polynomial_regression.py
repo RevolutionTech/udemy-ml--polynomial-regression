8b5b7917bba1862a9d30766a4aff4c1ed173e343
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-16 17:03:30 -0800

    Fit linear regression and polynomial regression to our dataset

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -10,3 +10,15 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
+
+# Fitting Linear Regression to our dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to our dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
