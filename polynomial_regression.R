9dc70b3b7b6dfe2010e0325218d20a4316063657
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-12 23:58:50 -0800

    Add a fourth degree to the polynomial regression model

--- polynomial_regression.R
+++ polynomial_regression.R
@@ -11,6 +11,7 @@ lin_reg = lm(formula = Salary ~ .,
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 
